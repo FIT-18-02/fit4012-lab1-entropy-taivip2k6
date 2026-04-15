32b09dd95f54bcf8fb7c06efe4a47d0136a9e5f5
taivip2k6 <taibonxu10001@gmail.com>
2026-04-15 15:53:37 +0700

    Update mod_inverse.cpp

--- src/mod_inverse.cpp
+++ src/mod_inverse.cpp
@@ -1,51 +1,36 @@
 #include <iostream>
-
 using namespace std;
 
-int gcd(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
-
-int extended_euclid(int a, int b, int &x, int &y) {
+int extended_gcd(int a, int b, int &x, int &y) {
     if (b == 0) {
-        x = 1;
-        y = 0;
+        x = 1; y = 0;
         return a;
     }
-
-    int x1 = 0, y1 = 0;
-    int g = extended_euclid(b, a % b, x1, y1);
+    int x1, y1;
+    int g = extended_gcd(b, a % b, x1, y1);
     x = y1;
     y = x1 - (a / b) * y1;
     return g;
 }
 
 int mod_inverse(int a, int m) {
-    // TODO(student): implement modular inverse using extended_euclid()
-    // If inverse does not exist, return -1.
-    (void)a;
-    (void)m;
-    return -1;
+    int x, y;
+    int g = extended_gcd(a, m, x, y);
+
+    if (g != 1) return -1;
+    return (x % m + m) % m;
 }
 
 int main() {
-    int a = 0, m = 0;
-    cout << "Nhap a, m: ";
+    int a, m;
     cin >> a >> m;
 
-    if (gcd(a, m) != 1) {
-        cout << "Khong ton tai nghich dao modulo vi gcd(a, m) != 1.\n";
-        return 0;
-    }
+    int res = mod_inverse(a, m);
 
-    int inv = mod_inverse(a, m);
-    cout << "Nghich dao cua " << a << " mod " << m << " la: " << inv << '\n';
-    cout << "Kiem tra: " << a << " * " << inv << " % " << m
-         << " = " << (1LL * a * inv % m) << '\n';
+    if (res == -1)
+        cout << "Not exist";
+    else
+        cout << res;
+
     return 0;
 }
