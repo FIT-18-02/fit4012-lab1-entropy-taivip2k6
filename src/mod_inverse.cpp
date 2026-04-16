5786cc164889ea5a64d294cae3d2574d0ef689a7
taivip2k6 <taibonxu10001@gmail.com>
2026-04-16 22:58:38 +0700

    Update mod_inverse.cpp

--- src/mod_inverse.cpp
+++ src/mod_inverse.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
+
 using namespace std;
 
-int extended_gcd(int a, int b, int &x, int &y) {
-    if (b == 0) {
-        x = 1;
-        y = 0;
-        return a;
+// Thuật toán Euclid mở rộng
+long long extended_gcd(long long a, long long b, long long &x, long long &y) {
+    if (a == 0) {
+        x = 0; y = 1;
+        return b;
     }
-    int x1, y1;
-    int g = extended_gcd(b, a % b, x1, y1);
-    x = y1;
-    y = x1 - (a / b) * y1;
-    return g;
+    long long x1, y1;
+    long long gcd = extended_gcd(b % a, a, x1, y1);
+    x = y1 - (b / a) * x1;
+    y = x1;
+    return gcd;
 }
 
-int mod_inverse(int a, int m) {
-    int x, y;
-    int g = extended_gcd(a, m, x, y);
+void modInverse(long long a, long long m) {
+    long long x, y;
+    long long g = extended_gcd(a, m, x, y);
+    if (g != 1) {
+        cout << "không tồn tại" << endl;
+    } else {
+        long long res = (x % m + m) % m;
+        cout << res << endl;
+    }
+}
 
-    if (g != 1) return -1;
-    return (x % m + m) % m;
+int main() {
+    long long a, m;
+    while (cin >> a >> m) {
+        modInverse(a, m);
+    }
+    return 0;
 }
